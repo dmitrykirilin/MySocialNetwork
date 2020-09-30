187201bdc72bf4733f4fafee4da7f8ff5c17949e
dmitrykirilin <demonus18@gmail.com>
2020-09-30 13:04:01 +0300

    Pagination added..

--- src/main/java/com/spring/MySpring/controller/BlogController.java
+++ src/main/java/com/spring/MySpring/controller/BlogController.java
@@ -7,6 +7,10 @@ import com.spring.MySpring.services.UserService;
 import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +21,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 @Controller
@@ -32,19 +37,22 @@ public class BlogController {
 
     @GetMapping("/main")
     public String allPostsPage(@AuthenticationPrincipal User currentUser,
-                           Model model){
-        Iterable<Post> posts = postRepository.findAll();
-        model.addAttribute("posts", posts);
+                               Model model,
+                               @PageableDefault(sort = {"id"}, page = 1, direction = Sort.Direction.DESC) Pageable pageable){
+        Page<Post> page = postRepository.findAll(pageable);
+        model.addAttribute("page", page);
+        model.addAttribute("url", "/main");
         return "main";
     }
 
     @GetMapping("/blog/{user}")
     public String userBlogPage(@AuthenticationPrincipal User currentUser,
                             @PathVariable @ModelAttribute User user,
-                            Model model){
+                            Model model,
+                            @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable){
 //        Hibernate.initialize(currentUser.getSubscribers());
 //        Hibernate.initialize(currentUser.getSubscriptions());
-        Iterable<Post> posts = postRepository.findByAuthor(user);
+        Iterable<Post> posts = postRepository.findByAuthor(user, pageable);
         model.addAttribute("isCurrent", user.equals(currentUser));
         model.addAttribute("isHaveSubscription", user.getSubscribers().contains(currentUser));
         model.addAttribute("subscriptionsCount", user.getSubscriptions().size());
--- src/main/java/com/spring/MySpring/repo/PostRepository.java
+++ src/main/java/com/spring/MySpring/repo/PostRepository.java
@@ -2,8 +2,13 @@ package com.spring.MySpring.repo;
 
 import com.spring.MySpring.models.Post;
 import com.spring.MySpring.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface PostRepository extends CrudRepository<Post, Integer> {
-    Iterable<Post> findByAuthor(User user);
+    Page<Post> findByAuthor(User user, Pageable pageable);
+
+    Page<Post> findAll(Pageable pageable);
 }
